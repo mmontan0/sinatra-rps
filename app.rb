f66241b388104cd43dfc3d235910f0f68097579b
mmontan0 <165187154+mmontan0@users.noreply.github.com>
2024-04-17 22:13:48 +0000

    piedra

--- app.rb
+++ app.rb
@@ -7,3 +7,33 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+ moves= ["rock", "paper", "scissors"]
+ comp_move=moves.sample
+ result_=""
+
+ if comp_move=="rock"
+  result_="tied"
+ elsif comp_move=="paper"
+  result_="won"
+ else
+  result_="lost"
+ end
+
+  "
+  <h2>
+  We played rock!
+  </h2>
+  <h2>
+  They played #{comp_move}!
+  </h2>
+ <h2>
+ We #{result_}!
+ </h2>
+
+  "
+end
+
+
+ 
